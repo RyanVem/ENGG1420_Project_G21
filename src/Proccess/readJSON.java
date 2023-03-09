198d7294f755b036f4e75e01331a226dce69734c
jdykst02 <jdykstra04@gmail.com>
2023-03-09 14:25:10 -0500

    Revert "Revert "Revert "entries class should be reading entries from json file and putting them in a list. May be some minor errors, needs to be tested with a better set up main class. """
    This reverts commit afa19d98feb854f3d491bb8ae2ccf340d86bb78c.

---
+++ src/Proccess/readJSON.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proccess;
+
+/**
+ *
+ * @author Paul's Laptop
+ */
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class readJSON {
+    
+}
