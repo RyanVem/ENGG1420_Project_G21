6f2133108e5a36d46a66eb6428bf5256590be8ed
RyanVem <ryanvemulapalli@gmail.com>
2023-03-09 14:58:19 -0500

    Adding NameFilter class

---
+++ src/Proccess/NameFilter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proccess;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ryan Vemulapalli
+ */
+public class NameFilter {
+    
+    public ArrayList<Object> NameFilter(ArrayList<Object> inputList){
+        
+        return inputList;
+    }
+    
+}
