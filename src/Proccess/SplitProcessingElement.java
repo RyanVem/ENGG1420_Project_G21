0a986b7191d8406acff2175b4046f31c23b9fd69
RyanVem <ryanvemulapalli@gmail.com>
2023-03-16 15:23:15 -0400

    Added imports

--- src/Proccess/SplitProcessingElement.java
+++ src/Proccess/SplitProcessingElement.java
@@ -5,18 +5,24 @@
 package Proccess;
 
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  *
  * @author ryanv
  */
 public class SplitProcessingElement {
+   
     public ArrayList<String> SplitProcessingElement(ArrayList<String> inputList, int Lines){
-        ArrayList<String> outputList = new ArrayList<String>();
+        ArrayList<String> filesList = new ArrayList<String>();
         
-        return outputList;
+        return filesList;
     }
-        
-        
     
 }
