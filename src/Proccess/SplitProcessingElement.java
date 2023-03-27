9c9521256c0418893a7ce8572dd181024f382c1a
RyanVem22 <ryanvemulapalli@hotmail.com>
2023-03-27 16:59:58 -0400

    Editing Split Processing Element

--- src/Proccess/SplitProcessingElement.java
+++ src/Proccess/SplitProcessingElement.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,21 +18,56 @@ import java.nio.file.Paths;
  *
  * @author ryanv
  */
-public class SplitProcessingElement {
-   
-    public ArrayList<String> SplitProcessingElement(ArrayList<String> inputList, int lines){
-        ArrayList<String> outputList = new ArrayList<String>();
-        
-        for (String entry : inputList) {
-            Path path = Paths.get(entry);
 
-            if (Files.isDirectory(path)) {
-                // Ignore directories
-                continue;
-            }
-        }
+public class SplitProcessingElement {
+    private ArrayList<String> inputList; 
+    private int lines;
             
-        return outputList;
+    public SplitProcessingElement(ArrayList<String> inputList, int lines){
+        this.inputList = inputList;
+        this.lines = lines;
+    }
+    
+    
+    public ArrayList<String> FileSeparater() throws IOException{
+        ArrayList<String> outputFiles = new ArrayList<String>();
+        
+        for (String file : inputList) {
+            File inputFile = new File(file);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            
+            String line;
+            int lineNumber = 1;
+            int partNumber = 1;
+            BufferedWriter writer = new BufferedWriter(new FileWriter(getPartFileName(inputFile, partNumber)));
+
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+
+                if (lineNumber == lines) {
+                    writer.close();
+                    outputFiles.add(getPartFileName(inputFile, partNumber));
+                    partNumber++;
+                    writer = new BufferedWriter(new FileWriter(getPartFileName(inputFile, partNumber)));
+                    lineNumber = 0;
+                }
+
+                lineNumber++;
+            }
+
+            writer.close();
+            reader.close();
+            outputFiles.add(getPartFileName(inputFile, partNumber));
+        }
+        return outputFiles;        
+    }
+    
+    
+    private static String getPartFileName(File inputFile, int partNumber) {
+        String fileNameWithoutExtension = inputFile.getName().substring(0, inputFile.getName().lastIndexOf('.'));
+        String fileExtension = inputFile.getName().substring(inputFile.getName().lastIndexOf('.'));
+        return fileNameWithoutExtension + "part" + partNumber + fileExtension;
     }
     
 }
