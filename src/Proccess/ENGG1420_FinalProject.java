ee69c23fc2840d51f498c68277f19c0b14c048e5
RyanVem <ryanvemulapalli@gmail.com>
2023-03-27 17:17:57 -0400

    Testing Split Processing Element

--- src/Proccess/ENGG1420_FinalProject.java
+++ src/Proccess/ENGG1420_FinalProject.java
@@ -4,6 +4,9 @@
  */
 package Proccess;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author ryanv, Jacob
@@ -13,8 +16,16 @@ public class ENGG1420_FinalProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        
+        ArrayList<String> FileTest = new ArrayList<String>();
+        FileTest.add("testfile.txt");
+        
+        SplitProcessingElement myElement = new SplitProcessingElement(FileTest, 2);
+        
+        myElement.FileSeparater();
+        
     }
     
 }
